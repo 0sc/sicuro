365c48b17356c403a29a8369c987ef3389bbb771
Osmond Oscar <osmond.oranagwa@andela.com>
2017-09-05 14:18:13 +0100

    add route handlers

---
+++ app/handlers.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/0sc/sicuro/app/vcs"
+	"github.com/0sc/sicuro/app/webhook"
+	"github.com/0sc/sicuro/ci"
+	"github.com/gorilla/sessions"
+	"github.com/gorilla/websocket"
+)
+
+type projectLogListing struct {
+	Name   string
+	Active bool
+}
+
+func indexPageHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index", nil)
+}
+
+func githubWebhookHandler(w http.ResponseWriter, r *http.Request) {
+	webhook.GithubWebhookHandler(r)
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		if _, ok := err.(websocket.HandshakeError); !ok {
+			log.Println(err)
+		}
+		return
+	}
+
+	var lastMod time.Time
+	if n, err := strconv.ParseInt(r.FormValue("lastMod"), 16, 64); err == nil {
+		lastMod = time.Unix(0, n)
+	}
+	logFile := logFilePathFromRequest("/ws/", r)
+	go writer(ws, lastMod, logFile)
+}
+
+func runCIHandler(w http.ResponseWriter, r *http.Request, session *sessions.Session) {
+	project := r.URL.Query().Get("project")
+	owner := r.URL.Query().Get("owner")
+	sha := r.URL.Query().Get("sha")
+	lang := r.URL.Query().Get("language")
+	url := r.URL.Query().Get("url")
+	repo := r.URL.Query().Get("repo")
+
+	webhook.ManualTrigger(project, owner, sha, lang, url)
+	http.Redirect(w, r, fmt.Sprintf("ci/%s", repo), 302)
+}
+
+func ciPageHandler(w http.ResponseWriter, r *http.Request) {
+	logFile := logFilePathFromRequest("/ci/", r)
+	fmt.Println("The logfile", logFile)
+	if _, err := os.Open(logFile); err != nil {
+		http.Error(w, "Not found", 404)
+		return
+	}
+	p, lastMod, err := readFileIfModified(logFile, time.Time{})
+	if err != nil {
+		p = []byte(err.Error())
+		lastMod = time.Unix(0, 0)
+	}
+
+	session, _ := sessionStore.Get(r, sessionName)
+	projectPath, _ := filepath.Rel("/ci/", r.URL.Path)
+	details := strings.Split(projectPath, "/")
+
+	var v = struct {
+		Owner         string
+		Project       string
+		Commit        string
+		Host          string
+		Data          template.HTML
+		LastMod       string
+		ProjectPath   string
+		Notifications []interface{}
+	}{
+		Owner:         details[0],
+		Project:       details[1],
+		Commit:        details[2],
+		Host:          r.Host,
+		Data:          template.HTML(p),
+		LastMod:       strconv.FormatInt(lastMod.UnixNano(), 16),
+		ProjectPath:   projectPath,
+		Notifications: session.Flashes(),
+	}
+	renderTemplate(w, "ci", &v)
+}
+
+func dashboardPageHandler(w http.ResponseWriter, r *http.Request, session *sessions.Session) {
+	tkn := accessTokenFromSession(session)
+	repos := getUserProjectsWithSubscriptionInfo(tkn)
+	info := struct {
+		FlashMsgs []interface{}
+		Repos     []RepoWithSubscriptionInfo
+	}{
+		FlashMsgs: session.Flashes(),
+		Repos:     repos,
+	}
+	session.Save(r, w)
+	renderTemplate(w, "dashboard", info)
+}
+
+func showPageHandler(w http.ResponseWriter, r *http.Request, session *sessions.Session) {
+	project := r.URL.Query().Get("project")
+	owner := r.URL.Query().Get("owner")
+	logDir := filepath.Join(ci.LogDIR, owner, project)
+
+	logs := listProjectLogsInDir(logDir)
+	info := struct{ Logs []projectLogListing }{logs}
+	renderTemplate(w, "show", info)
+}
+
+func ghSubscribe(w http.ResponseWriter, r *http.Request, session *sessions.Session) {
+	project := r.URL.Query().Get("project")
+	owner := r.URL.Query().Get("owner")
+	redirPath := "/dashboard"
+
+	payload := vcs.GithubRequestParams{
+		Owner:       owner,
+		Repo:        project,
+		CallbackURL: webhookPath,
+		Creds:       os.Getenv("GITHUB_WEBHOOK_SECRET"),
+	}
+
+	err := newGithubClientFromSession(session).Subscribe(payload)
+	if err != nil {
+		log.Println("Error while creating webhook", err)
+		session.AddFlash("An error occurred. The project might have already been subscribed.")
+	} else {
+		session.AddFlash("Sicro is now watching: ", project)
+		redirPath = fmt.Sprintf("/show?project=%s&owner=%s", project, owner)
+	}
+
+	session.Save(r, w)
+	http.Redirect(w, r, redirPath, 302)
+}
